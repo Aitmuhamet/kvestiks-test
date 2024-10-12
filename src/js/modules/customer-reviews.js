4acadff1aaa958b8aaa91d538bb1c7a1be5237a6
Aitmukhamet Khojakeldiyev <xa210296@gmail.com>
2024-10-12 17:35:20 +0500

    BugFix: swiper pagination

--- src/js/modules/customer-reviews.js
+++ src/js/modules/customer-reviews.js
@@ -1,14 +1,13 @@
 import Swiper from "swiper";
 import { Navigation, Pagination } from "swiper/bundle";
 
-const swiperContainer = document.querySelector(".content-container");
 const swiperSlides = document.querySelectorAll(".swiper-slide");
 const slidesNumber = document.querySelector(".slider-info__total");
+const slideCount = swiperSlides.length;
+const fraction = document.getElementById("fraction");
+fraction.textContent = `1 / ${slideCount}`
 
-const currentSlideIndex = document.querySelector(".slider-info__current");
-
-console.log(swiperSlides.length);
-slidesNumber.innerHTML = swiperSlides.length;
+slidesNumber.innerHTML = slideCount;
 
 let swiper = new Swiper(".mySwiper", {
   slidesPerView: 1.2,
@@ -19,37 +18,21 @@ let swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
   breakpoints: {
-    400: {
-      slidesPerView: 1.3,
+    360: {
+      slidesPerView: 1.2,
       spaceBetween: 20,
     },
     450: {
       slidesPerView: 1.4,
       spaceBetween: 20,
     },
-    500: {
-      slidesPerView: 1.6,
-      // spaceBetween: 20,
-    },
-    550: {
-      slidesPerView: 1.8,
-      // spaceBetween: 50,
+  },
+  on: {
+    slideChange: () => {
+      fraction.textContent = `${swiper.realIndex + 1} / ${slideCount}`;
     },
   },
 });
 
-// Получаем все кнопки пагинации
-const paginationButtons = document.querySelectorAll(
-  ".swiper-pagination-bullet"
-);
-
-// Для каждой кнопки добавляем обработчик клика
-paginationButtons.forEach((button, index) => {
-  button.addEventListener("click", () => {
-    // Прокручиваем слайдер так, чтобы выбранный слайд стал первым
-    swiper.slideTo(index, 200);
-    currentSlideIndex.innerHTML = index + 1; // 500 - это длительность анимации
-  });
-});
 
 export default swiper;
